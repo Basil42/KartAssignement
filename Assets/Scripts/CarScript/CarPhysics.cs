ef933468b46c90f9dd2301c98fcb23bfa08f6e10
Basil42 <32388487+Basil42@users.noreply.github.com>
2023-09-28 16:58:38 +0200

    pulled out some physics stuff
    removed some of the physics before moving to a new simulation

--- Assets/Scripts/CarScript/CarPhysics.cs
+++ Assets/Scripts/CarScript/CarPhysics.cs
@@ -1,28 +1,23 @@
 using System;
-using System.Globalization;
+using System.Collections.Generic;
 using JetBrains.Annotations;
 using UnityEngine;
-using UnityEngine.Assertions;
 using UnityEngine.InputSystem;
 using UnityEngine.Serialization;
 
+
 [RequireComponent(typeof(Rigidbody2D))]
 public class CarPhysics : MonoBehaviour
-{
+{ 
+    //single rigidbody idea, direction dependant grip and existence of VelocityAtPoint method from: https://www.youtube.com/watch?v=CdPYlj5uZeI&t=6s&ab_channel=ToyfulGames
     private Rigidbody2D _rb;
-    [SerializeField] private Rigidbody2D frontContact;
-    private Transform _frontContactTransform;
-    [SerializeField] private Rigidbody2D backContact;
-    private Transform _backContactTransform;
+    [SerializeField] private List<Transform> contactPatches;//one per "tire"
     [SerializeField] private float maxAcceleration = 5f;
     private float _currentAccelerationInput;
 
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
-        Assert.IsNotNull(frontContact);
-        _frontContactTransform = frontContact.transform;
-        _backContactTransform = backContact.transform;
     }
     
     [UsedImplicitly]
@@ -30,36 +25,20 @@ public class CarPhysics : MonoBehaviour
     {
         _currentAccelerationInput =  value.Get<float>();
     }
-    
     private void FixedUpdate()
     {
         
-        FrontContactPhysicsUpdate();
-        BackContactPhysicsUpdate();
-    }
-    
-    [Tooltip("The maximum amount of speed the contact patch can transfer forward in a second")] 
-    [SerializeField] private float gripForceLimit = 10f;//you could make this mass dependant 
-    private void BackContactPhysicsUpdate()
-    {
-        var velocity = backContact.velocity;
-        var orthogonalDirection = _backContactTransform.right;
-        var orthogonalSpeed = Vector2.Dot(velocity,orthogonalDirection);
-        var speedTransferredForward = Mathf.Clamp( orthogonalSpeed,-gripForceLimit,gripForceLimit);
-        velocity += (Vector2)(-_backContactTransform.up) * (Mathf.Abs(speedTransferredForward) * Time.fixedDeltaTime) - (Vector2)orthogonalDirection * (speedTransferredForward * Time.fixedDeltaTime);
-        backContact.velocity = velocity;//this change gets instantly "digested" by the heavier car body-
-        //unhinged force based solution
-        
-        
     }
 
-    private void FrontContactPhysicsUpdate()
+    private void PatchPhysicsUpdate(Transform frontContactTransform)
     {
-        //Default to grip regime
-        var frontContactVelocity = frontContact.velocity;
-        var frontContactForward = _frontContactTransform.up;
-        frontContact.velocity = frontContactVelocity.magnitude * frontContactForward;//the physics system might not be able to resolve this
-        //Mathf.Cos(Vector2.Angle(frontContactVelocity, frontContactForward)) *
-        frontContact.AddForce(frontContactForward * (_currentAccelerationInput * maxAcceleration * Time.fixedDeltaTime));
+        throw new NotImplementedException();
     }
+}
+
+public struct ContactPatches
+{
+    public Vector2 LocalPosition;
+    public AnimationCurve DirectionalGripCurve; 
+    public float LateralSpeedGripLimit;//beyond which grip is lost
 }
--- Assets/Scripts/CarScript/CarSteering.cs
+++ Assets/Scripts/CarScript/CarSteering.cs
@@ -13,14 +13,13 @@ public class CarSteering : MonoBehaviour
     [FormerlySerializedAs("maxTurningPower")] [SerializeField] private float maxTurningForce = 90f;
     [SerializeField] private float turningPower = 10f;
     private float _currentTurningInput;
-    private float _maxSteeringAngle = 45f;
+    [SerializeField] private float maxSteeringAngle = 45f;
     private const float SteeringAnglePadding = 0.1f;
 
     private void Awake()
     {
         Assert.IsNotNull(frontContactRb);
         _rb = GetComponent<Rigidbody2D>();
-        _maxSteeringAngle = frontContactRb.GetComponent<HingeJoint2D>().limits.max- SteeringAnglePadding;
     }
 
     [UsedImplicitly]
@@ -33,6 +32,6 @@ public class CarSteering : MonoBehaviour
     {
         //TODO: clamping and resetting to neutral, pad the allowed steering to avoid the hinge "launching" the car
         var contactRotation =  frontContactRb.rotation - (_currentTurningInput * maxTurningForce * Time.fixedDeltaTime);
-        frontContactRb.MoveRotation(Mathf.Clamp(contactRotation, -_maxSteeringAngle, _maxSteeringAngle));
+        frontContactRb.MoveRotation(contactRotation);//this is of course a world rotation and is incorrect
     }
 }
